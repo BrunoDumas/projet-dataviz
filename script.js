ff182f95e1ce1dbaf0275dba523fc3bfcff70619
Bruno Dumas <bruno.dumas@etu.univlyon1.fr>
2017-01-05 20:37:57 +0100

    Main view OK (TODO: interractions between views)

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 /* Display setup */
-var displayArea = {width: 960, height: 500},
+var displayArea = {width: 500, height: 500},
 	pathviewHeight = 50,
 	margin = {top: 20, right: 20, bottom: 20, left: 20},
-	r = displayArea.height * 0.9,
-	x = d3.scaleLinear().range([0, r]),
-  y = d3.scaleLinear().range([0, r]),
 	body = d3.select("body"),
 	svgPathView = body.append("svg")
-        .attr("width", displayArea.width)
+        .attr("width", displayArea.width * 2)
         .attr("height", pathviewHeight),
     svgOverview = body.append("svg")
-        .attr("width", displayArea.width / 2)
-        .attr("height", displayArea.height - pathviewHeight),
+        .attr("width", displayArea.width)
+        .attr("height", displayArea.height),
 	svgMainView = body.append("svg")
-        .attr("width", displayArea.width / 2)
-        .attr("height", displayArea.height - pathviewHeight),
+        .attr("width", displayArea.width)
+        .attr("height", displayArea.height),
 	pathview = { // File path view
 		width: svgPathView.attr("width") - margin.left - margin.right,
 		height: svgPathView.attr("height") - margin.top - margin.bottom,
@@ -29,8 +26,32 @@ var displayArea = {width: 960, height: 500},
 		width: svgMainView.attr("width") - margin.left - margin.right,
 		height: svgMainView.attr("height") - margin.top - margin.bottom,
 		g: svgMainView.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")"),
-	};
-mainview.g.append("rect").attr("x", -margin.left).attr("y",  -margin.top).attr("width", mainview.width + margin.right + margin.left).attr("height", mainview.height + margin.top + margin.bottom).style("fill", "white").style("stroke", "black")
+	},
+	r = Math.min(mainview.height, mainview.width),
+	x = d3.scaleLinear().range([0, r]),
+	y = d3.scaleLinear().range([0, r]);
+
+mainview.g
+	.append("rect")
+		.attr("x", -margin.left)
+		.attr("y",  -margin.top)
+		.attr("width", mainview.width + margin.right + margin.left)
+		.attr("height", mainview.height + margin.top + margin.bottom)
+		.style("fill", "white")
+		.style("stroke", "black")
+
+overview.g
+	.append("rect")
+		.attr("x", -margin.left)
+		.attr("y",  -margin.top)
+		.attr("width", overview.width + margin.right + margin.left)
+		.attr("height", overview.height + margin.top + margin.bottom)
+		.style("fill", "white")
+		.style("stroke", "black")
+
+var tooltip = d3.select('body').append('div')
+	.attr('class', 'hidden tooltip');
+
 
 var pack = d3.pack()
     .size([mainview.width - 2, mainview.height - 2])
@@ -80,31 +101,60 @@ function processData(error, data){
 	});
 
     var root = stratify(data2)
-		.sum(function(d) { return d.size; })
+		.sum(function(d) { return d.size;})
 		.sort(function(a, b) { return b.size - a.size; });
 
 	pack(root);
 
-    displayTree(root);
+    displayMainView(root);
+    displayPathView(root);
 }
 
 function hovered(hover) {
-  return function(d) {
-    d3.selectAll(d.ancestors().map(function(d) { return d.node; })).classed("node--hover", hover);
-  };
+	return function(d) {
+	d3.selectAll(d.ancestors().map(function(d) { return d.node; })).classed("node--hover", hover);
+
+	tooltip.classed('hidden', !hover);
+	};
+}
+
+function displayTooltip(d){
+	var mouse = d3.mouse(svgMainView.node()).map(function(d2) {
+		return parseInt(d2);
+	});
+	tooltip.classed('hidden', false)
+        .attr('style', 'left:' + (mouse[0] + 60 + mainview.width) + 'px; top:' + (mouse[1] + 70) + 'px')
+        .html("<table>" +
+			"<tr>" +
+				"<td>" + (d.data.filetype == "d" ? "Dir" : "File") + "</td>" +
+				"<td>" + d.data.filename + "</td>" +
+			"</tr>" +
+			"<tr>" +
+				"<td>Owner</td>" +
+				"<td>" + d.data.owner + "</td>" +
+			"</tr>" +
+			"<tr>" +
+				"<td>Size</td>" +
+				"<td>" + readableFileSize(d.data.size) + "</td>" +
+			"</tr>" +
+			"<tr>" +
+				"<td>Date</td>" +
+				"<td>" + displayDate(d.data.timestamp * 1000) + "</td>" +
+			"</tr>" +
+		"</table>");
 }
 
 /* Display functions */
-function displayTree(root) {
+function displayMainView(root) {
 	var node = svgMainView.select("g")
 		.selectAll("g")
 		.data(root.descendants())
 		.enter().append("g")
-			//.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
 			.attr("class", function(d) { return "node" + (!d.children ? " node--leaf" : d.depth ? "" : " node--root"); })
 			.each(function(d) { d.node = this; })
 			.on("mouseover", hovered(true))
 			.on("mouseout", hovered(false))
+			.on("mousemove", displayTooltip)
 			.on("click", function(d) { return zoom(node == d ? node : d); });
 
 	node.append("circle")
@@ -114,76 +164,80 @@ function displayTree(root) {
 		.attr("r", function(d) { return d.r; })
 		.style("fill", function(d) { return mainview.color(d.depth); });
 
-	var leaf = node.filter(function(d) { return !d.children; });
-
-	leaf.append("clipPath")
-			.attr("id", function(d) { return "clip-" + d.filename; })
-		.append("use")
-			.attr("xlink:href", function(d) { return "#node-" + d.filename + ""; });
-
-	leaf.append("text")
-			.attr("clip-path", function(d) { return "url(#clip-" + d.filename + ")"; })
-		.selectAll("tspan")
-		.data(function(d) { return d.data.filename.substring(d.data.filename.lastIndexOf(".") + 1).split(/(?=[A-Z][^A-Z])/g); })
-		.enter().append("tspan")
-			.attr("x", 0)
-			.attr("y", function(d, i, nodes) { return 13 + (i - nodes.length / 2 - 0.5) * 10; })
-			.text(function(d) { return d; });
-
-	node.append("title")
-		.text(function(d) { return d.filename + "\n" + readableFileSize(d.size); });
+	node.append("text")
+			.attr("x", function(d) { return d.x })
+			.attr("y", function(d) { return d.y })
+			.text(function(d) { return d.data.filename; })
+			.style("text-anchor", "middle");
 
 	d3.select(window).on("click", function() { zoom(root); });
-
-    /*var treeView = g.append("g")
-        .classed("treeView", true);
-
-    treeView.selectAll("circle.treeNode")
-        .data([data])
-        .enter()
-        .append("circle")
-            .classed("treeNode", true)
-            .attr("cx", 10)
-            .attr("cy", overview.height / 2)
-            .attr("r", 10);
-    treeView.selectAll("text.treeNodeText")
-        .data([data])
-        .enter()
-        .append("text")
-            .classed("treeNodeText", true)
-            .text(function (d) {
-                return d.filename;
-            })
-            .attr("x", 10)
-            .attr("y", overview.height / 2 + 32)
-            .style("font-size", 20);
-*/
+	zoom(root);
 
 }
 
 // Adapted from : http://mbostock.github.io/d3/talk/20111116/#10
 function zoom(d, i) {
-  var k = r / d.r / 2;
-  x.domain([d.x - d.r, d.x + d.r]);
-  y.domain([d.y - d.r, d.y + d.r]);
+	var k = r / d.r / 2;
+	x.domain([d.x - d.r, d.x + d.r]);
+	y.domain([d.y - d.r, d.y + d.r]);
 
-  var t = svgMainView.transition()
-      .duration(d3.event.altKey ? 7500 : 750);
+	var t = mainview.g.transition()
+		.duration(500);
 
-  t.selectAll("circle")
-      .attr("cx", function(d) { return x(d.x); })
-      .attr("cy", function(d) { return y(d.y); })
-      .attr("r", function(d) { return k * d.r; });
+	t.selectAll("circle")
+		.attr("cx", function(d) { return x(d.x); })
+		.attr("cy", function(d) { return y(d.y); })
+		.attr("r", function(d) { return k * d.r; });
 
-  t.selectAll("title")
-      .attr("x", function(d) { return x(d.x); })
-      .attr("y", function(d) { return y(d.y); })
-      .style("opacity", function(d) { return k * d.r > 20 ? 1 : 0; });
+	t.selectAll("text")
+		.attr("x", function(d) { return x(d.x); })
+		.attr("y", function(d) { return y(d.y); })
+		.style("opacity", function(d) { return k * d.r > 20 ? 1 : 0; });
 
-  node = d;
-  d3.event.stopPropagation();
+	node = d;
+	d3.event && d3.event.stopPropagation();
+	displayPathView(d)
 }
 
+function getTextWidth(text, fontSize, fontFace) {
+        var a = document.createElement('canvas');
+        var b = a.getContext('2d');
+        b.font = fontSize + 'px ' + fontFace;
+        var mesure = b.measureText(text);
+        return mesure.width;
+}
 
+function displayPathView(selectedNode) {
+	var directory = selectedNode;
+	var nodes = [];
+	while(typeof directory != "undefined" && directory){
+		nodes.push(directory);
+		directory = directory.parent;
+	}
+	nodes.reverse();
+	var trunk = pathview.g
+		.select("g")
+		.data(nodes);
+
+	trunk.exit().remove();
+
+	var enter = trunk.enter().append("g")
+	/*enter.append("polygon")
+		.attr("points", breadcrumbPoints)
+		.style("fill", "lightgrey");*/
+
+	/*enter.append("text")
+		.attr("x", function(d, i){
+			if(i != 0){
+				return enter.data()[i - 1] +
+			}
+
+			return 10;
+		})
+		.attr("y", pathviewHeight - 20 )
+		.style("font-size", 20)
+		.attr("text-anchor", "left")
+		.text(function(d) { return d.data.filename; });*/
+}
 
 init();
